c10db5d50c7df1c77c5446396fd58b7a12f60c46
AliBardide5124 <alibardide5124@gmail.com>
2019-11-13 22:41:01 -0800

    Create CardViewPlus class

---
+++ cardviewplus/src/main/java/com/alibardide5124/cardviewplus/CardViewPlus.java
@@ -0,0 +1,22 @@
+package com.alibardide5124.cardviewplus;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+
+public class CardViewPlus extends CardView {
+
+    public CardViewPlus(@NonNull Context context) {
+        super(context);
+    }
+    public CardViewPlus(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public CardViewPlus(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    
+}
